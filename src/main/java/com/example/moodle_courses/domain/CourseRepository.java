613022d339972ea4ef49ba53f3e35ecd2ba15309
TomasKukk <tomsu_kukk@hotmail.com>
2019-01-18 19:21:33 +0200

    Updates

---
+++ src/main/java/com/example/moodle_courses/domain/CourseRepository.java
@@ -0,0 +1,5 @@
+package com.example.moodle_courses.domain;
+
+public interface CourseRepository extends CrudRepository<MoodleCourse, Long> {
+	
+}
--- src/main/java/com/example/moodle_courses/web/MainController.java
+++ src/main/java/com/example/moodle_courses/web/MainController.java
@@ -1,5 +1,22 @@
 package com.example.moodle_courses.web;
 
-public class MainController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.moodle_courses.domain.CourseRepository;
+import com.example.moodle_courses.domain.MoodleCourse;
+
+@Controller
+public class MainController {
+	
+	@Autowired
+	private CourseRepository repo;
+	
+	@RequestMapping("/index")
+	public String showAllCourses (Model model) {
+		model.addAttribute(new MoodleCourse());
+		return "index";
+	}
 }
