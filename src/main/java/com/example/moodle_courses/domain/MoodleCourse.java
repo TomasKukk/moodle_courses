702527654069dabb1e871656dd2d88ec77044ae3
TomasKukk <tomsu_kukk@hotmail.com>
2019-01-18 19:07:23 +0200

    Updates

---
+++ src/main/java/com/example/moodle_courses/domain/MoodleCourse.java
@@ -0,0 +1,40 @@
+package com.example.moodle_courses.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class MoodleCourse {
+	
+	@Id
+	@GeneratedValue(strategy=Generation.Type.AUTO)
+	private long id;
+	private String courseName;
+	private String courseLink;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getCourseName() {
+		return courseName;
+	}
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+	public String getCourseLink() {
+		return courseLink;
+	}
+	public void setCourseLink(String courseLink) {
+		this.courseLink = courseLink;
+	}
+	@Override
+	public String toString() {
+		return "MoodleCourse [id=" + id + ", courseName=" + courseName + ", courseLink=" + courseLink + "]";
+	}
+	
+}
---
+++ src/main/java/com/example/moodle_courses/web/MainController.java
@@ -0,0 +1,5 @@
+package com.example.moodle_courses.web;
+
+public class MainController {
+
+}
