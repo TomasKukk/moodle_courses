ce6f23c6ff3c486e174d43861cbcf2695b6000b6
TomasKukk <tomsu_kukk@hotmail.com>
2019-01-18 18:16:27 +0200

    First commit

---
+++ src/main/java/com/example/moodle_courses/MoodleCoursesApplication.java
@@ -0,0 +1,14 @@
+package com.example.moodle_courses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodleCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodleCoursesApplication.class, args);
+	}
+
+}
+
