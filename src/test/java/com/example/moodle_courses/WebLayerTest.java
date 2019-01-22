1b6598553e8dc3126f1a1ba36072d86f4cacce9c
TomasKukk <tomsu_kukk@hotmail.com>
2019-01-22 13:47:09 +0200

    finally working web layer test

--- src/test/java/com/example/moodle_courses/WebLayerTest.java
+++ src/test/java/com/example/moodle_courses/WebLayerTest.java
@@ -2,22 +2,25 @@ package com.example.moodle_courses;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockitoTestExecutionListener;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
 
 import com.example.moodle_courses.web.MainController;
 
 @RunWith(SpringRunner.class)
-//@WebMvcTest(MainController.class)
+@WebMvcTest(MainController.class)
+@TestExecutionListeners(MockitoTestExecutionListener.class)
 public class WebLayerTest {
-//	@Autowired
-//	private MockMvc mock;
+	@Autowired
+	private MockMvc mock;
+	
 	
 	@Test
 	public void testTesting() {
